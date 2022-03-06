d7728f37300fad95af82d0789a9279e0b8e56636
adimancifi <46333102+adimancifi@users.noreply.github.com>
2022-03-07 01:31:40 +0800

    Update auth_footer.php

--- l-app/views/backend/auth_footer.php
+++ l-app/views/backend/auth_footer.php
@@ -1,6 +1,5 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');?>
 	<script src="<?=content_url('plugins/bootstrap/js/bootstrap.min.js');?>"></script>
-	<script src="<?=content_url('plugins/pace/pace.min.js');?>"></script>
 	<script src="<?=content_url('plugins/feather/feather.min.js');?>"></script>
 	<script src="<?=content_url('plugins/slimscroll/jquery.slimscroll.min.js');?>"></script>
 	<script src="<?=content_url('plugins/perfect-scrollbar/perfect-scrollbar.js');?>"></script>
