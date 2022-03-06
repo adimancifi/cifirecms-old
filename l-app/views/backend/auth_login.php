74b2c6107dba7bb729b0de63facb2743ae08de32
adimancifi <46333102+adimancifi@users.noreply.github.com>
2022-03-07 01:31:37 +0800

    Update auth_login.php

--- l-app/views/backend/auth_login.php
+++ l-app/views/backend/auth_login.php
@@ -5,15 +5,21 @@
 		<h4 class="mg-b-10 tx-semiboldX"><?=lang_line('signin_title');?></h4>
 		<p><?=lang_line('signin_welcome');?></p>
 	</div>
-	<div class="err"><?= $this->cifire_alert->show('login'); ?></div>
+	<div class="err"><?=$this->cifire_alert->show('login');?></div>
 	<div class="card auth-box">
 		<?=form_open('', 'id="form-login" class="login-form" autocomplete="off"');?>
 		<div class="form-group">
 			<label><?=lang_line('username')?></label>
-			<input id="username" type="text" name="<?=$input_uname;?>" class="form-control input-username" maxlength="20"/>
+			<input id="username" type="text" name="<?=$input_uname;?>" class="form-control input-username" maxlength="20" required>
 		</div>
-		<div class="input-password"></div>
+		<div class="form-group">
+			<label><?=lang_line('password');?></label>
+			<input type="password" name="<?=$input_pwd?>" id="password" class="form-control" maxlength="20" required>
+		</div>
+		<button type="submit" class="btn btn-brand btn-block mg-t-20"><?=lang_line('button_login');?></button>
+		<div class="text-center mg-t-20">
+			<a href="<?=admin_url('forgot')?>"><?=lang_line('forgot_password');?>?</a>
+		</div>
 		<?=form_close();?>
 	</div>
 </div>
-<?=$script?>
