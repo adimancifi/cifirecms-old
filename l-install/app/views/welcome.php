5f3837b90db0e436d8d8e72b9130b6d9e6f958fc
adimancifi <46333102+adimancifi@users.noreply.github.com>
2022-03-07 00:54:48 +0800

    merge repository

---
+++ l-install/app/views/welcome.php
@@ -0,0 +1,19 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');?>
+<div id="container">
+	<div class="text-center">
+		<h2>CiFireCMS Installation Page</h2>
+		<h4>Click Start Installation to Continue</h4>
+	</div>
+	<div class="license">
+		<div id="body">
+			<textarea class="form-control" style="background-color: #fafafa;" readonly><?=license();?></textarea>
+		</div>
+	</div>
+	<hr/>
+	<div class="action">
+		<form method="POST">
+			<input type="hidden" name="act" value="start">
+			<button type="submit" class="btn btn-lg btn-success">Start Installation</button>
+		</form>
+	</div>
+</div>
