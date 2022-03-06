3b89119d7c0414f12796d6ee789d6eba65820446
adimancifi <46333102+adimancifi@users.noreply.github.com>
2022-03-07 00:49:06 +0800

    merge repository

---
+++ l-app/logs/log-2022-03-06.php
@@ -0,0 +1,6 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2022-03-06 21:14:14 --> Severity: Notice --> Trying to access array offset on value of type null E:\www-laragon\cifirecms-v2\l-app\views\themes\sovi\post.php 143
+ERROR - 2022-03-06 21:14:17 --> Severity: Notice --> Trying to access array offset on value of type null E:\www-laragon\cifirecms-v2\l-app\views\themes\sovi\post.php 143
+ERROR - 2022-03-06 21:14:20 --> Severity: Notice --> Trying to access array offset on value of type null E:\www-laragon\cifirecms-v2\l-app\views\themes\sovi\post.php 143
+ERROR - 2022-03-06 21:14:23 --> Severity: Notice --> Trying to access array offset on value of type null E:\www-laragon\cifirecms-v2\l-app\views\themes\sovi\post.php 143
